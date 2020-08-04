7787d4d5ebf85700be00d6c465e659ed3c2f8294
bryan <info@catmanstudios.com>
2020-08-03 22:29:36 -0400

    get full predict result

--- app/server.py
+++ app/server.py
@@ -66,7 +66,7 @@ async def analyze(request):
     img_data = await request.form()
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
-    pred_class,pred_idx,outputs = learn.predict(img)[0]
+    pred_class,pred_idx,outputs = learn.predict(img)
     prob = round(float(outputs[int(pred_idx)]) * 100)
     # prediction = learn.predict(img)[0]
     # return JSONResponse({'result': str(prediction)})
